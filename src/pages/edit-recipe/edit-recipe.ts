850e1775a0e77f941ab8ab9d5d4fe84e90f9c65d
HuyTrinh <dinhhuyams@gmail.com>
2018-02-14 23:19:50 +0200

    set up new recipe form

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {ActionSheetController, IonicPage, NavController, NavParams} from 'ionic-angular';
+import {FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
 
-/**
- * Generated class for the EditRecipePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
@@ -14,12 +9,50 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'edit-recipe.html',
 })
 export class EditRecipePage {
+  mode = 'New';
+  selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private actionSheetController: ActionSheetController) {
+    this.mode = navParams.get('mode');
+    this.initializeForm();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad EditRecipePage');
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'description': new FormControl(null, Validators.required),
+      'difficulty': new FormControl('Medium', Validators.required)
+    });
+  }
+
+  onSubmit() {
+   console.log(this.recipeForm);
+  }
+
+  onManageIngredients() {
+   const actionSheet = this.actionSheetController.create({
+     title: 'What do you want to do?',
+     buttons: [
+       {
+         text: 'Add Ingredient',
+         handler: () => {
+
+         }
+       },
+       {
+         text: 'Remove all Ingredients',
+         role: 'destructive',
+         handler: () => {
+
+         }
+       },
+       {
+         text: 'Cancel',
+         role: 'cancel'
+       }
+     ]
+   });
   }
 
 }
