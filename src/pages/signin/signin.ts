aa27e60fdad50537229ab27ce8b050833f2c50ad
HuyTrinh <dinhhuyams@gmail.com>
2018-02-19 23:45:34 +0200

    create a signin page

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
 /**
  * Generated class for the SigninPage page.
@@ -18,8 +19,9 @@ export class SigninPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+
+  onSignin(form: NgForm) {
+
   }
 
 }
