0a10c2819206e5e828c3b237cf1ae061ee77f8ef
HuyTrinh <dinhhuyams@gmail.com>
2018-02-19 21:08:40 +0200

    output recipes

--- src/pages/recipes/recipes.ts
+++ src/pages/recipes/recipes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {EditRecipePage} from "../edit-recipe/edit-recipe";
+import {Recipe} from "../../models/recipe";
+import {RecipesService} from "../../services/recipes";
 
 /**
  * Generated class for the RecipesPage page.
@@ -15,12 +17,21 @@ import {EditRecipePage} from "../edit-recipe/edit-recipe";
   templateUrl: 'recipes.html',
 })
 export class RecipesPage {
+  recipes: Recipe[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private recipesService: RecipesService) {
+  }
+
+  ionViewWillEnter() {
+    this.recipes = this.recipesService.getRecipes();
   }
 
   onNewRecipe() {
     this.navCtrl.push(EditRecipePage, {mode: 'New'});
+  }
+
+  onLoadRecipe() {
+
   }
 
 }
